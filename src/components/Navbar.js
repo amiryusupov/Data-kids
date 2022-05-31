3022ae1707ba8894ff528599f1bcfd245f7179ef
Amir <amiryusupov.070@gmail.com>
2022-05-31 18:53:52 +0500

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, {useState , useEffect} from 'react'
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {setLang} from "../redux/reducers/LangReducer";
-import store from "../redux/store";
 
 
 

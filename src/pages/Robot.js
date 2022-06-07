9ad150ec50788f703cb08655848d73bd28351cfb
Amir <amiryusupov.070@gmail.com>
2022-06-07 16:55:12 +0500

    commit

--- src/pages/Robot.js
+++ src/pages/Robot.js
@@ -1,43 +1,43 @@
 import React from 'react';
 
 function Robot() {
-    let data = [
-        {
-            id: 1,
-            name_uz: "Matematika va fizika bo'limlarini o'rganamiz",
-            name_ru: "Мы изучаем математический и физический факультеты",
-            desc_uz: "Dasturlash va matematika bizning asosiy faoliyatimizdir.Biz Lego to'plamlari bilan mexanika bo'limini va Arduino-ni o'rganamiz",
-            desc_ru: "Программирование и математика - наша основная деятельность. Мы изучаем механику и Arduino с наборами Lego.",
-        },
-        {
-            id: 2,
-            name_uz: "Dasturlash va informatika",
-            name_ru: "Программирование и информатика",
-            desc_uz: "Dasturlash faoliyatimizning asosiy qismlaridan biridir. Biz Lego bilan dasturlashni boshlaymiz - eng oddiy blok sxemasidan boshlab o'tamiz va keyinchalik yuqori darajali dasturlash tillari bilan ham shug'illanamiz",
-            desc_ru: "Программирование — одна из ключевых частей нашей работы. Начинаем программировать с Лего - начинаем с простейшей блок-схемы и далее уже занимаемся языками программирования высокого уровня",
-        },
-        {
-            id: 3,
-            name_uz: "Mexanika (loyihalash asoslari)",
-            name_ru: "Механика (основы дизайна)",
-            desc_uz: "Lego o'quv to'plamlari bizga mexanizmlarning harakat tamoyilarini, transmissiya  nima ekanligi, va turli xildagi mexanizmlarni ishlashdagi boshlang'ich bilim va ko'nikmalarni beradi",
-            desc_ru: "Учебные наборы Lego дают нам базовые знания и навыки в принципах движения механизмов, что такое трансмиссия и как управлять различными механизмами",
-        },
-        {
-            id: 4,
-            name_uz: "Fikrlashni rivojlantirish algoritmlash",
-            name_ru: "Алгоритм развития мышления",
-            desc_uz: "Mantiqiy vazifalar va jumboqlar yechishda taffakurni rivojlantirishga yordam beradi, javob izlash va xulosa chiqarish qobilyatini rivojlantiradi ",
-            desc_ru: "Помогает развивать мышление при решении логических задач и головоломок, развивает умение искать ответы и делать выводы",
-        },
-        {
-            id: 5,
-            name_uz: "Jamoa bo'lib ishlashni o'rgatamiz",
-            name_ru: "Учим работать в команде",
-            desc_uz: "Jamoa bo'lib ishlash maqsadlarga samarali erishish uchun mihimdir. Raqobatbardoshlikni hibsga olgan holda bolaning bilim olishga qiziqishini oshirish orqali yaxshi natijalarga erishamiz",
-            desc_ru: "Помогает развивать мышление при решении логических задач и головоломок, развивает умение искать ответы и делать выводы",
-        },
-    ]
+    {/*let data = [*/}
+    {/*    {*/}
+    {/*        id: 1,*/}
+    {/*        name_uz: "Matematika va fizika bo'limlarini o'rganamiz",*/}
+    {/*        name_ru: "Мы изучаем математический и физический факультеты",*/}
+    {/*        desc_uz: "Dasturlash va matematika bizning asosiy faoliyatimizdir.Biz Lego to'plamlari bilan mexanika bo'limini va Arduino-ni o'rganamiz",*/}
+    {/*        desc_ru: "Программирование и математика - наша основная деятельность. Мы изучаем механику и Arduino с наборами Lego.",*/}
+    //     },
+    //     {
+    //         id: 2,
+    {/*        name_uz: "Dasturlash va informatika",*/}
+    {/*        name_ru: "Программирование и информатика",*/}
+    {/*        desc_uz: "Dasturlash faoliyatimizning asosiy qismlaridan biridir. Biz Lego bilan dasturlashni boshlaymiz - eng oddiy blok sxemasidan boshlab o'tamiz va keyinchalik yuqori darajali dasturlash tillari bilan ham shug'illanamiz",*/}
+    {/*        desc_ru: "Программирование — одна из ключевых частей нашей работы. Начинаем программировать с Лего - начинаем с простейшей блок-схемы и далее уже занимаемся языками программирования высокого уровня",*/}
+    {/*    },*/}
+    {/*    {*/}
+    //         id: 3,
+    //         name_uz: "Mexanika (loyihalash asoslari)",
+    {/*        name_ru: "Механика (основы дизайна)",*/}
+    {/*        desc_uz: "Lego o'quv to'plamlari bizga mexanizmlarning harakat tamoyilarini, transmissiya  nima ekanligi, va turli xildagi mexanizmlarni ishlashdagi boshlang'ich bilim va ko'nikmalarni beradi",*/}
+    {/*        desc_ru: "Учебные наборы Lego дают нам базовые знания и навыки в принципах движения механизмов, что такое трансмиссия и как управлять различными механизмами",*/}
+    {/*    },*/}
+    {/*    {*/}
+    //         id: 4,
+    //         name_uz: "Fikrlashni rivojlantirish algoritmlash",
+    {/*        name_ru: "Алгоритм развития мышления",*/}
+    //         desc_uz: "Mantiqiy vazifalar va jumboqlar yechishda taffakurni rivojlantirishga yordam beradi, javob izlash va xulosa chiqarish qobilyatini rivojlantiradi ",
+    //         desc_ru: "Помогает развивать мышление при решении логических задач и головоломок, развивает умение искать ответы и делать выводы",
+    //     },
+    //     {
+    //         id: 5,
+    //         name_uz: "Jamoa bo'lib ishlashni o'rgatamiz",
+    //         name_ru: "Учим работать в команде",
+    //         desc_uz: "Jamoa bo'lib ishlash maqsadlarga samarali erishish uchun mihimdir. Raqobatbardoshlikni hibsga olgan holda bolaning bilim olishga qiziqishini oshirish orqali yaxshi natijalarga erishamiz",
+    //         desc_ru: "Помогает развивать мышление при решении логических задач и головоломок, развивает умение искать ответы и делать выводы",
+    //     },
+    // ]
     return (
         <div className="robot">
             <div className="container">

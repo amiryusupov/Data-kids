d88ba909154a779a6b1d86a62263cac63bf6e700
Amir <amiryusupov.070@gmail.com>
2022-06-14 17:41:33 +0500

    commit

--- src/pages/Robot.js
+++ src/pages/Robot.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useSelector} from "react-redux";
 import {robot} from "../helpers/data";
+import teacher_img from "../assets/img/teacher-img4.png";
 
 function Robot() {
     let lang = useSelector(state => state.lang.lang)
@@ -23,6 +24,18 @@ function Robot() {
                                 </div>
                             )
                         })}
+                    </div>
+                </div>
+                <div className="robot__teach">
+                    <div className="robot__teach-img">
+                        <img src={teacher_img} alt=""/>
+                        <div className="robot__teach-shape1"></div>
+                        <div className="robot__teach-shape2"></div>
+                    </div>
+                    <div className="robot__teach-text">
+                        <div className="robot__teach-title">{lang === "uz" ? "Mirzobek Atayev" : "Мирзобек Атаев"}</div>
+                        <div className="robot__teach-content">{lang === "uz" ? "\"Robototexnika\" kursi mentori" : "Наставник курс робототехники"}</div>
+                        <div className="robot__teach-desc">{lang === "uz" ? "Oliy ma'lumotli, sohada 6 yillik tajribaga ega. \"Turin\" politexnika universitetini tamomlagan. ERIELL kompaniyasida injiner (muhandis) bo'lib ishlagan. \"Int-tech\", \"Technolab\" MCHJ asoschisi. Bundan tashqari \"Smarthouse\" loyihasini asoschisi. O'quvchi dronlar loyihasi bilan shug'ullangan" : "Высшее образование, опыт работы по специальности 6 лет. Окончил Туринский политехнический университет. Работал инженером в ERIELL. Основатель Int-tech, ООО «Технолаб». Он также является основателем проекта Smarthouse. Студент был вовлечен в проект беспилотника"}</div>
                     </div>
                 </div>
             </div>
